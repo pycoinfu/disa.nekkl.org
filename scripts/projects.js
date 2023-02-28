6558d42e41f2671f613d5bc26be3d21d558c9b4c
unknown <matvejbuc192@gmail.com>
2023-02-28 13:46:24 +0800

    added an animation for the desc

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,21 +1,39 @@
-const projectElements = document.getElementsByClassName("project");
+const projectElements = document.querySelectorAll("img");
 const projectDescriptionElements = document.getElementsByClassName("project-desc");
 const projectDecsriptions = {
-    "pygame-platformer": "E",
+    "pygame-platformer": "A platformer game I've been working on",
     "pgfs": "I am one of the contributors for the Pygame Guide for Sneks: \
             a guide for the Pygame library, that lets you make games with Python.",
     "dave's-anniversary": "I am one of the contributors for Dave's Anniversary, \
             a game created for the Pygame Community Summer Jam 2022."
 }
 
-for (let i = 0; i < projectElements.length; i++) {
-    element = projectElements[i]
-    id = projectElements[i].id;
+function animateDescription(element) {
+    splitText = projectDecsriptions[element.id].split("");
 
-    element.addEventListener("mouseover", () => {
-        projectDescriptionElements[i].textContent = projectDecsriptions[id];
+    for (let i = 0; i < splitText.length; i++) {
+        element.innerHTML += `<span>${splitText[i]}</span>`
+    }
+
+    let char = 0;
+    let timer = setInterval(() => {
+        const span = element.querySelectorAll("span")[char];
+        span.classList.add("fade");
+        char++;
+        if (char === splitText.length) {
+            clearInterval(timer);
+            timer = null;
+            return;
+    }
+    }, 1);
+    
+}
+
+for (let i = 0; i < projectElements.length; i++) {
+    projectElements[i].addEventListener("mouseover", () => {
+        animateDescription(projectDescriptionElements[i])
     })
-    element.addEventListener("mouseout", () => {
+    projectElements[i].addEventListener("mouseout", () => {
         projectDescriptionElements[i].textContent = "";
     })
 }
